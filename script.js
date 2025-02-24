06df1510eb429c19d976daed85a6ace681dad0bc
shivuni nishnath <shivuninishnath@gmail.com>
2025-02-24 14:39:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.apply').addEventListener('click', function() {
+    alert('Thank you for applying! We will get back to you soon.');
+});
+
+document.querySelector('.learn').addEventListener('click', function() {
+    alert('Learn more about our internship by visiting our website.');
+});
